0f8a2a14555f0cfbd62a041c40f6c0f9fe7dbe47
Georgii <georgy.epischin@yandex.ru>
2024-09-30 16:38:11 +0300

    main

---
+++ lib/models/player_card.dart
@@ -0,0 +1,12 @@
+class PlayerCard {
+  int playerCardId;
+  final String footballerName;
+  final String description;
+  final String photoUrl;
+
+  PlayerCard(
+      {required this.playerCardId,
+      required this.footballerName,
+      required this.description,
+      required this.photoUrl});
+}
